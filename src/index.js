0253e016f709dcfb395610200bc1542616bbdcde
Carlos <solrak.m@gmail.com>
2019-06-25 23:20:46 -0500

    10- Create Card element

--- src/index.js
+++ src/index.js
@@ -5,33 +5,9 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const user={
-    firstName: 'Carlos',
-    lastName: 'Guevara',
-    avatar:'https://purepng.com/public/uploads/large/purepng.com-batman-logobatmansuperherocomicdc-comicsbob-kanebat-manbruce-wayne-1701528523430sg9aj.png',
-}
-
-function getName(user){
-    return user.firstName +' '+ user.lastName
-}
-
-function getGreeting(user){
-    if(user){
-       return <h1>Hello {getName(user)}</h1>
-    }
-    return <h1>Hello Stranger</h1>
-}
-const name = 'Carlos'
-
-//const element = <div>{getGreeting()}</div>
-const element=(
-    <div>
-        <h1>{getGreeting(user)}</h1>
-        <img src={user.avatar} height="100px"/>
-    </div>
-)
+import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(element,container)
+ReactDOM.render(<Card/>,container)
