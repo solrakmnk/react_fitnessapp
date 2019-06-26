eab2c04024e4d87789b98144ffd6ee08df7eafef
Carlos A. Guevara <solrak.m@gmail.com>
2019-06-26 10:14:09 -0500

    props

--- src/index.js
+++ src/index.js
@@ -10,4 +10,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(<Card/>,container)
+ReactDOM.render(<Card 
+    title="Technique Guides"
+    description="Learn amazing street workout and calisthenics"
+    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+    leftColor="#A74CF2"
+    rightColor="#617BFB"
+/>, container)
