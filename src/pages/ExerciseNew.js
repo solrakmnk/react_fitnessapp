445ce0df4b8c388f08a805f5b570841bb218652a
Carlos A. Guevara <solrak.m@gmail.com>
2019-07-01 09:30:12 -0500

    Eventos

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 class ExersiceNew extends React.Component {
+/*    constructor(props){
+        super(props)
+        this.handleClick=this.handleClick.bind(this)
+    }
+    handleClick(){
+        console.log(this)
+    }*/
+    handleClick=()=>{
+        console.log(this)
+    }
     render (){
-        return 'ExerciseNew'
+        return(
+            <button onClick={this.handleClick}>
+                Send
+            </button>
+        )
     }
 }
 
