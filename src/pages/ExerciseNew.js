f85eed7e9e29a103aaded4b84419aa1cd6944e98
Carlos A. Guevara <solrak.m@gmail.com>
2019-07-01 10:58:15 -0500

    Formulario nuevo Ejercicio

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -1,23 +1,89 @@
 import React from 'react'
 
-class ExersiceNew extends React.Component {
-/*    constructor(props){
-        super(props)
-        this.handleClick=this.handleClick.bind(this)
+class ExerciseNew extends React.Component {
+
+    state = {}
+
+    handleSubmit = e => {
+        e.preventDefault()
+        console.log(this.state)
     }
-    handleClick(){
-        console.log(this)
-    }*/
-    handleClick=()=>{
-        console.log(this)
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
-    render (){
-        return(
-            <button onClick={this.handleClick}>
-                Send
-            </button>
+
+    render(){
+        return (
+            <div className="container">
+            <form 
+                onSubmit={this.handleSubmit}
+            >
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="title" 
+                        name="title"
+                        onChange={this.handleChange}
+                        value={this.state.title}
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="description" 
+                        name="description"
+                        onChange={this.handleChange}
+                        value={this.state.description}
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="img" 
+                        name="img"
+                        onChange={this.handleChange}
+                        value={this.state.img}
+                    />
+                </div>
+                <div className="form-row">
+                    <div className="col">
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            placeholder="leftColor" 
+                            name="leftColor"
+                            onChange={this.handleChange}
+                            value={this.state.leftColor}
+                        />
+                    </div>
+                    <div className="col">
+                        <input 
+                            type="text" 
+                            className="form-control"
+                            placeholder="rightColor" 
+                            name="rightColor"
+                            onChange={this.handleChange}
+                            value={this.state.rightColor}
+                        />    
+                    </div>
+                </div>
+                
+                <button 
+                    type="submit" 
+                    className="btn btn-primary"
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
         )
     }
 }
 
-export default ExersiceNew
+export default ExerciseNew
