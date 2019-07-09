2e67779ec38dd864648baf9f6f5da88c4d85fd93
Carlos A. Guevara <solrak.m@gmail.com>
2019-07-09 17:47:40 -0500

    Loader and error

---
+++ src/pages/500.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FatalErrorImg from '../images/500.png'
+
+const FatalError=()=>(
+    <div className="text-center">
+        <h1>Fatal Error</h1>
+        <img src={FatalErrorImg}/>
+
+    </div>
+)
+
+export default FatalError
--- src/pages/Exercises.js
+++ src/pages/Exercises.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ExerciseList from '../components/ExerciseList'
 import Welcome from '../components/Welcome'
-import AddButton from '../components/AddButton';
+import AddButton from '../components/AddButton'
+import Loading from '../components/Loading'
+import FatalError from './500'
 
 class Exercises extends React.Component {
         state={
-            data:[]
+            data:[],
+            loading: true,
+            error: null
         }
         
         async componentDidMount(){
@@ -13,12 +17,28 @@ class Exercises extends React.Component {
         }
 
         fetchExercises = async() =>{
+            try{
             let res = await fetch('http://localhost:8000/api/exercises')
             let data = await res.json()
-            this.setState({data})
+            this.setState({
+                data,
+                loading:false
+            })
+            } catch(error){
+                this.setState({
+                    loading:false,
+                    error
+                })
+            }
         }
 
         render() {
+            if(this.state.loading){
+                return <Loading/>
+            }
+            if(this.state.error){
+                return <FatalError/>
+            }
         return(
             <div>
                 <Welcome username="Carlos"/>
